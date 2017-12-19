dd682d2f0bc03ccae4f3d942d977e1f09e556c26
bagefly <huangxhe@126.com>
2017-12-19 20:16:07 +0800

    vue

---
+++ client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <NavHead/>
+    <BreadCrumb>商品列表</BreadCrumb>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import '@/assets/css/app.css'
+import NavHead from '@/components/NavHead'
+import BreadCrumb from '@/components/BreadCrumb'
+export default {
+  name: 'app',
+  components: {
+    NavHead,
+    BreadCrumb
+  }
+}
+</script>
+
+<style>
+
+</style>
